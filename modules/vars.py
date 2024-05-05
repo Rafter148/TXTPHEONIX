23441f4c4e8dd404523e22a9908c5c4dc6f2bb3a
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-05-06 00:06:08 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,10 +1,3 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 6251412907))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6389388334").split()))
+api_id = "28660719"
+api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
+bot_token = "7193875965:AAFsiKojQb5-7BYt5SLDYuWjIOO3oXUUuMQ"
